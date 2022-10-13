e5a5b7685f1135064e5fd7cd90e021ca560dfba2
Han <mngceg@gmail.com>
2022-10-13 20:28:12 +0200

    Clases y objetos de empresa terminado

--- clases_Y_Obj_Empresa.php
+++ clases_Y_Obj_Empresa.php
@@ -10,127 +10,163 @@
 
 <body>
     <?php
-    class Persona {
-        public $nombre;
-        public $apellido;
-        public $edad;
+class Persona
+{
+    protected $nombre;
+    protected $apellido;
+    protected $edad;
 
-        function __construct($nombre, $apellido, $edad){
-            $this->nombre = $nombre;
-            $this->apellido = $apellido;
-            $this->edad = $edad;
-        }
-
-        function __toString(){
-            return "Hola soy " . $this->nombre . " " . $this->apellido . " tengo " . $this->edad ." años";
-        }
+    public function __construct($nombre, $apellido, $edad)
+    {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->edad = $edad;
     }
 
-    class Cliente extends Persona {
-        public $saldo;
-    
-        function __construct($nombre, $apellido, $edad, $saldo){
-            parent::__construct($nombre, $apellido, $edad);
-            $this->saldo = $saldo;
-        }
-
-        function __toString(){
-            return parent::__toString() . " y tengo " . $this->saldo . " euros";
-        }
+    public function __toString()
+    {
+        return "Hola, soy " . $this->nombre . " " . $this->apellido . " tengo " . $this->edad . " años, ";
     }
-
-    class Empleado extends Persona {
-        public $sueldo;
-        public $antiguedad;
-
-        function __construct($nombre, $apellido, $edad, $sueldo, $antiguedad){
-            parent::__construct($nombre, $apellido, $edad);
-            $this->sueldo = $sueldo;
-            $this->antiguedad = $antiguedad;
-        }
-
-        function __toString(){
-            return parent::__toString() . " cobro " . $this->sueldo . " euros y tengo una antigüedad de " . $this->antiguedad . " días";
-        }
-
-        function __getAntiguedad(){
-            return $this->antiguedad;
-        }
-    }
-
-    class Empresa {
-        private $nombre;
-        private $empleados = array();
-        private $clientes = array();
-
-        function __construct($nombre, $empleados, $clientes){
-            $this->nombre=$nombre;
-            $this->empleados=$empleados;
-            $this->clientes=$clientes;
-        }
-
-        function verClientes(){
-            while(current($this->clientes)){
-                echo current($this->clientes) . "<br>";
-                next($this->clientes);
-            }
-        }
-
-        function verEmpleados(){
-            
-            asort($this->empleados, SORT_STRING);
-            while(current($this->empleados)){
-                echo current($this->empleados) . "<br>";
-                next($this->empleados);
-            }
-        }
-
-        function añadirEmpleados($nif, $emp){
-            $this->empleados[$nif] = $emp; 
-        }
-
-        function masAntiguo(){
-            $antiguedades = array();
-            foreach($this->empleados as $k => $v){
-                $antiguedades[$k] = $v->__getAntiguedad();
-            }
-            asort($antiguedades);
-            end($antiguedades);
-            echo $this->empleados[key($antiguedades)];
-        }
-
-        function __toString(){
-            return "Somos " . $this->nombre . " tenemos " . count($this->empleados) ." empleados y " . count($this->clientes) . " clientes";
-        }
 }
-    // $p1 = new Persona("Mengcheng", "Han", 20);
-    
-    $misClientes = array(
-        new Cliente("Mengcheng", "Han", 20, 150), 
-        new Cliente("Raúl", "Castaño", 27, 645), 
-        new Cliente("Kein", "Besil", 25, 123)
-    );
-    
-    $misEmpleados = array(
-        "1234A" => new Empleado("Kap", "Uyo o", 20, 1800, 120), 
-        "1234B" => new Empleado("Alba", "Sánchez", 21, 1050, 650), 
-        "1234C" => new Empleado("Filo", "DaPuta", 15, 2500, 455), 
-        "1234D" => new Empleado("Roland", "Vallès", 33, 1520, 342)
-    );
-     
-    $empresa = new Empresa("Retrete SL", $misEmpleados, $misClientes);
+
+class Empleado extends Persona
+{
+    private $sueldo;
+    private $antiguedad;
+
+    public function __construct($nombre, $apellido, $edad, $sueldo, $antiguedad)
+    {
+        parent::__construct($nombre, $apellido, $edad);
+        $this->sueldo = $sueldo;
+        $this->antiguedad = $antiguedad;
+    }
+
+    public function __toString()
+    {
+        return parent::__toString() . " cobro " . $this->sueldo . " euros y tengo una antigüedad de " . $this->antiguedad . " días";
+    }
+
+    public function getAntiguedad()
+    {
+        return $this->antiguedad;
+    }
+
+    public function __getSueldo()
+    {
+        return $this->sueldo;
+    }
+
+    public function setSueldo($sueldo): self
+    {
+        $this->sueldo = $sueldo;
+        return $this;
+    }
+}
+
+class Empresa
+{
+    private $nombre;
+    private $empleados = array();
+
+    public function __construct($nombre, $empleados)
+    {
+        $this->nombre = $nombre;
+        $this->empleados = $empleados;
+    }
+
+    public function verEmpleados()
+    {
+        asort($this->empleados, SORT_STRING);
+        while (current($this->empleados)) {
+            echo current($this->empleados) . "<br>";
+            next($this->empleados);
+        }
+    }
+
+    public function añadirEmpleados($nif, $emp)
+    {
+        return ($this->empleados[$nif] = $emp) ? true : false;
+    }
+
+    public function masAntiguo()
+    {
+        $antiguedades = array();
+        foreach ($this->empleados as $k => $v) {
+            $antiguedades[$k] = $v->getAntiguedad();
+        }
+        asort($antiguedades);
+        end($antiguedades);
+        return $this->empleados[key($antiguedades)];
+    }
+
+    public function masNuevo()
+    {
+        $antiguedades = array();
+        foreach ($this->empleados as $k => $v) {
+            $antiguedades[$k] = $v->getAntiguedad();
+        }
+        asort($antiguedades);
+        reset($antiguedades);
+        return $this->empleados[key($antiguedades)];
+    }
+
+    public function antiguedadMedia()
+    {
+        $antiguedades = array();
+        foreach ($this->empleados as $k => $v) {
+            $antiguedades[] = $v->getAntiguedad();
+        }
+        return "La antiguedad media es de " . array_sum($antiguedades) / count($antiguedades) . " días";
+    }
+
+    public function sueldoMedio()
+    {
+        $sueldos = array();
+        foreach ($this->empleados as $k => $v) {
+            $sueldos[] = $v->__getSueldo();
+        }
+        echo "El sueldo medio es de " . array_sum($sueldos) / count($sueldos) . " euros";
+    }
+
+    public function aPagarAHacienda()
+    {
+        $sueldos = array();
+        foreach ($this->empleados as $k => $v) {
+            if ((int)$v->__getSueldo() > 3000) {
+                $sueldos[$k] = $v->__getSueldo();
+            }
+        }
+        asort($sueldos, SORT_DESC);
+        foreach ($sueldos as $key => $value) {
+            echo $this->empleados[$key] . "<br>";
+        }
+    }
+
+    public function __toString()
+    {
+        return "Somos " . $this->nombre . " tenemos " . count($this->empleados) . " empleados.";
+    }
+}
+
+$misEmpleados = array(
+    "1234A" => new Empleado("Kap", "Uyo o", 20, 4800, 120),
+    "1234B" => new Empleado("Alba", "Sánchez", 21, 1050, 650),
+    "1234C" => new Empleado("Filo", "DaPuta", 15, 6500, 455),
+    "1234D" => new Empleado("Roland", "Vallès", 33, 1520, 342)
+);
+
+    $empresa = new Empresa("Retrete SL", $misEmpleados);
+    echo $empresa->verEmpleados() . "<br>";
     $emp = new Empleado("Freya", "Vallès", 31, 1400, 20);
     $empresa->añadirEmpleados("1234G", $emp);
     echo $empresa . "<br>";
-    echo "Estos son mis clientes" . "<br>";
-    echo  $empresa->verClientes() . "<br>";
-    echo  $empresa->verEmpleados() . "<br>" ;
-    echo $empresa->masAntiguo();
-
-    
-    // echo $p1 . "<br>";
-    // echo $c1 . "<br>";
-?>
+    echo $empresa->verEmpleados() . "<br>";
+    echo $empresa->masAntiguo() . "<br>";
+    echo $empresa->masNuevo() . "<br>";
+    echo $empresa->sueldoMedio() . "<br>";
+    echo $empresa->antiguedadMedia() . "<br>";
+    print_r($empresa->aPagarAHacienda());
+    ?>
 </body>
 
 </html>
