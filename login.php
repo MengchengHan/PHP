e5dc5bbabea2addc63d6eebc0e996d841a027a7a
Alu2daw <meng.cheng@educa.madrid.org>
2022-11-04 13:55:45 +0100

    conexión con la base de datos hecho

--- login.php
+++ login.php
@@ -1,16 +1,21 @@
 <?php
+
     $mysqli = new mysqli('localhost', 'root', '', 'credenciales');
-    $user = 'Han';
-    $query = 'SELECT pass FROM CREDENCIALES WHERE user LIKE ' . $uservf;                
-    //$pass = $_POST["pass"];
-    echo $query;
-    // if(mysqli_connect_errno()){
-    //     echo 'Ha fallado la conexión a la base de datos.';
-    // } else {
-    //     $resultado = $mysqli->query($query);
-    //     print_r(mysqli_fetch_row($resultado));
-    //     //header("Location:welcome.html");
-    //     //} else{
-    //         header("Location:user_pass_incorrect.html");
-    // }
+    $user = $_POST['user'];
+    $query = "SELECT pass FROM CREDENCIALES WHERE user LIKE '$user'";                
+    $pass = $_POST['pass'];
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(mysqli_connect_errno()){
+            echo 'Ha fallado la conexión a la base de datos.';
+        } else {
+            $resultado = $mysqli->query($query);
+            $query_pass = mysqli_fetch_column($resultado);
+            if($pass == $query_pass){
+                header("Location: welcome.html");
+            } else{
+                header("Location: user_pass_incorrect.html");
+            }
+        }
+    }
 ?>
