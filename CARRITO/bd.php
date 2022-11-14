bd2ea6776fe9705705ebcda7b0e2a9cb3970ae9d
Alu2daw <meng.cheng@educa.madrid.org>
2022-11-14 10:21:31 +0100

    bd corregido

--- CARRITO/bd.php
+++ CARRITO/bd.php
@@ -61,7 +61,7 @@ function cargar_categoria($codCat){
 function cargar_productos_categoria($codCat){
 	$res = leer_config(dirname(__FILE__)."/configuracion.xml", dirname(__FILE__)."/configuracion.xsd");
 	$bd = new PDO($res[0], $res[1], $res[2]);	
-	$sql = "select * from productos where categoria  = $codCat";	
+	$sql = "select * from productos where CodCat  = $codCat";	
 	$resul = $bd->query($sql);	
 	if (!$resul) {
 		return FALSE;
@@ -78,7 +78,7 @@ function cargar_productos($codigosProductos){
 	$res = leer_config(dirname(__FILE__)."/configuracion.xml", dirname(__FILE__)."/configuracion.xsd");
 	$bd = new PDO($res[0], $res[1], $res[2]);
 	$texto_in = implode(",", $codigosProductos);
-	$ins = "select * from productos where codProd in($texto_in)";
+	$ins = "select * from productos where codProd in('$texto_in')";
 	$resul = $bd->query($ins);	
 	if (!$resul) {
 		return FALSE;
